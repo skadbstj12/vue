02262eb33fa0251f261fef0fdd48038abc7ff7b0
skadbstj12 <163810643+skadbstj12@users.noreply.github.com>
2024-06-12 16:54:17 +0900

    11

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,3 +1,182 @@
+<script setup>
+import { headerNav } from '@/constants'
+</script>
+
 <template>
-    Header
+    <header id="header" role="banner">
+        <div class="header__inner">
+            <h1 class="header__logo">
+                <a href="#">portfolio<em>vue.js</em></a>
+            </h1>
+            <nav class="header__nav" :class="{ show: isNavVisible }" role="navigation" aria-label="메인 메뉴">
+                <ul>
+                    <li v-for="(nav, key) in headerNav" :key="key">
+                        <a :href="nav.url" @click="scrollLink($event)">{{ nav.title }}</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__nav__mobile" id="headerToggle" aria-controls="primary-menu" aria-expanded="false" role="button" tabindex="0" @click="toggleNav">
+                <span></span>
+            </div>
+        </div>
+    </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isNavVisible: false
+        }
+    },
+    methods: {
+        toggleNav() {
+            this.isNavVisible = !this.isNavVisible
+        },
+        scrollLink(e) {
+            e.preventDefault()
+            const target = e.target.getAttribute('href')
+            const offsetTop = document.querySelector(target).offsetTop
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/mixin.scss';
+#header {
+    @include position-fixed;
+    z-index: 10000;
+
+    .header__inner {
+        @include flex-between;
+        background-color: rgba(241, 235, 235, 0.848);
+        backdrop-filter: blur(15px);
+        padding: 1rem;
+
+        .header__logo {
+            font-size: 2rem;
+            text-align: center;
+            text-transform: uppercase;
+            line-height: 1;
+
+            em {
+                font-size: 10px;
+                display: block;
+                color: var(--white200);
+            }
+        }
+
+        .header__nav {
+            @media (max-width: 800px) {
+                display: none;
+
+                &.show {
+                    display: block;
+
+                    ul {
+                        display: block;
+                        position: absolute;
+                        right: 0;
+                        top: 68px;
+                        background-color: rgba(166, 99, 99, 0.1);
+                        backdrop-filter: blur(15px);
+                        z-index: 10000;
+                        min-width: 150px;
+                        padding: 20px 0;
+
+                        li {
+                            display: block;
+                            text-align: right;
+
+                            a {
+                                display: inline-block;
+                                padding: 10px;
+                            }
+                        }
+                    }
+                }
+
+                &.show + .header__nav__mobile span::before {
+                    width: 20;
+                }
+                &.show + .header__nav__mobile span::after {
+                    width: 20;
+                }
+            }
+
+            li {
+                display: inline;
+
+                a {
+                    text-transform: uppercase;
+                    font-size: 1rem;
+                    padding: 14px;
+                    position: relative;
+                    font-weight: 400;
+
+                    &::before {
+                        content: '';
+                        height: 1px;
+                        width: calc(100% - 28px);
+                        background-color: var(--white);
+                        position: absolute;
+                        left: 14px;
+                        bottom: 10px;
+                        transform: scaleX(0);
+                        transition: all 0.3s;
+                    }
+
+                    &:hover::before {
+                        transform: scaleX(1);
+                    }
+                }
+            }
+        }
+        .header__nav__mobile {
+            display: none;
+            width: 40px;
+            height: 40px;
+            cursor: pointer;
+
+            @media (max-width: 800px) {
+                display: block;
+            }
+
+            span {
+                display: block;
+                width: 40px;
+                height: 2px;
+                background-color: var(--white);
+                margin-top: 19px;
+                position: relative;
+
+                &::before {
+                    content: '';
+                    width: 40px;
+                    height: 2px;
+                    background-color: var(--white);
+                    position: absolute;
+                    right: 0;
+                    top: 6px;
+                    transition: width 0.3s;
+                }
+                &::after {
+                    content: '';
+                    width: 40px;
+                    height: 2px;
+                    background-color: var(--white);
+                    position: absolute;
+                    right: 0;
+                    bottom: 6px;
+                    transition: width 0.3s;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -1,3 +1,170 @@
 <template>
-intro
+    <section id="intro">
+        <div class="intro__inner">
+            <h2 class="intro__title">develop port</h2>
+            <div class="intro__lines" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <div class="intro__text">
+                <div class="text">
+                    <div>talent is</div>
+                    <div>found at the end of the</div>
+                    <div>effort</div>
+                </div>
+                <div class="img">
+                    <img src="https://images.unsplash.com/photo-1595956553066-fe24a8c33395?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                </div>
+            </div>
+            <div class="intro__lines bottom" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+        </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+    #intro{
+        height: 100vh;
+
+        .intro__inner{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: flex-start;
+            justify-content: flex-end;
+            flex-direction: column;
+            padding: 16px;
+
+            .intro__title {
+                color: #96b4d1;
+                margin: 0 auto;
+                font-size: 10vw;
+                line-height: 1;
+                font-weight: 800;
+                white-space: nowrap;
+                text-transform: uppercase;
+                letter-spacing: -0.3vw;
+            }
+
+            .intro__lines {
+                    width: 100%;
+
+            .line {
+                display: block;
+                width: 100%;
+                height: 1px;
+                background-color: var(--black);
+                margin-bottom: 0.2vw;
+                box-shadow: 0 20px 20px  #000000;
+
+                &:nth-child(1) {
+                    height: 0;
+                    
+                }
+
+                &:nth-child(2) {
+                    height: 0;
+                }
+
+                &:nth-child(3) {
+                  height: 0;
+                }
+
+                &:nth-child(4) {
+                    background-color: rgba(24, 24, 24, 0.35);
+                    backdrop-filter: blur(1px);
+                    height: 8px;
+                    margin: 0 auto;
+                    border-radius: 120px 120px 0 0;
+                    width: 93.3%;
+                }
+
+                &:nth-child(5) {
+                    background-color: rgba(24, 24, 24, 0.4);
+                    backdrop-filter: blur(1px);
+                    height: 12px;
+                    margin: 0 auto;
+                    border-radius: 120px 120px 0 0;
+                    width: 94.7%;
+                }
+
+                &:nth-child(6) {
+                    background-color: rgba(24, 24, 24, 0.5);
+                    backdrop-filter: blur(1px);
+                    height: 14px;
+                    margin: 0.02rem auto;
+                    border-radius: 100px 100px 0 0;
+                    width: 96.1%;
+                }
+
+                &:nth-child(7) {
+                background-color: rgba(24, 24, 24, 0.65);
+                backdrop-filter: blur(5px);
+                height: 18px;
+                margin: 0 auto;
+                width: 98.3%;
+                border-radius: 30px 30px 0 0;
+                }
+            }
+            &.bottom {
+                        display: none;
+                    }
+        }
+
+
+            
+            .intro__text{
+                border-radius: 15px 15px 15px 15px;
+                width: 100%;
+                height: 40vh;
+                background-color: var(--black);
+                color: var(--white);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                position: relative;
+
+                .text{
+                    font-size: 3vw;
+                    line-height: 1;
+                    font-weight: 900;
+                    text-transform: uppercase;
+                    text-decoration: underline;
+                    text-align: center;
+                    position: relative;
+                    z-index: 100;
+                    transition: all 0.3s;
+                    cursor: all-scroll;
+                }
+
+                .img{
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%, -50%);
+                    width: 30vh;
+                    height: 30vh;
+                    border-radius: 50%;
+                    overflow: hidden;
+                    filter: grayscale(100%);
+                    transition: all 0.3s;
+                    object-position: 10px;
+                }
+            }
+        }
+
+    }
+</style>
